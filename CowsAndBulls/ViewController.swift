4c79d8ff586265c92f6f9a24b91dbce9d899c419
Kristiyan Butev <kristiyanbutev@simeons-macbook-pro.local>
2018-10-02 11:10:51 +0300

    Initial Commit

---
+++ CowsAndBulls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CowsAndBulls
+//
+//  Created by Kristiyan Butev on 2.10.18.
+//  Copyright © 2018 Kristiyan Butev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
